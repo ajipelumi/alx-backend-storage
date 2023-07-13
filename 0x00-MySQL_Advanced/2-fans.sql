f088f16a8850dcb50801f1c0b610cc073fda4a8e
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-13 02:56:16 +0100

    handled grouping

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 -- ordered by the number of fans
 
 -- Select the origin and the number of fans
-SELECT origin, COUNT(fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 -- Group by origin
 GROUP BY origin
