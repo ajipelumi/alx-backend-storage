b82be30bd9f186cc2086347b2d5266971350df1d
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-13 02:04:49 +0100

    created users table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,9 @@
 -- This script creates a table user with a unique email
 
 -- Create a table user
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
     PRIMARY KEY (id)
 );
