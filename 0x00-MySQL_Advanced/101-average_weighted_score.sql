8ec954976cec58086ca943f9dac21705d704736e
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-15 18:08:00 +0100

    implemented cursor and handler

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -9,33 +9,30 @@ BEGIN
     DECLARE user_id INT;
     DECLARE average_weighted_score FLOAT;
 
-    -- Get the first user_id
-    SELECT `id` INTO user_id
-    FROM `users`
-    ORDER BY `id`
-    LIMIT 1;
+    -- Declare a cursor to iterate over all users
+    DECLARE done INT DEFAULT FALSE;
+    DECLARE cur CURSOR FOR SELECT id FROM users;
+    -- Declare a handler to catch the end of the cursor
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 
-    -- Loop through all users
-    WHILE user_id IS NOT NULL DO
-        -- Get the average weighted score
-        SELECT SUM(`score` * `projects`.`weight`) / SUM(`projects`.`weight`) INTO average_weighted_score
-        FROM `corrections`
-        JOIN `projects`
-        ON `corrections`.`project_id` = `projects`.`id`
-        GROUP BY `corrections`.`user_id`
-        HAVING `corrections`.`user_id` = user_id;
+    -- Open the cursor
+    OPEN cur;
 
-        -- Update users table
-        UPDATE `users`
-        SET `average_score` = average_weighted_score
-        WHERE `id` = user_id;
+    -- Iterate over all users
+    read_loop: LOOP
+        -- Fetch the user id
+        FETCH cur INTO user_id;
 
-        -- Get the next user_id
-        SELECT `id` INTO user_id
-        FROM `users`
-        WHERE `id` > user_id
-        ORDER BY `id`
-        LIMIT 1;
-    END WHILE;
+        -- If the cursor is empty, leave the loop
+        IF done THEN
+            LEAVE read_loop;
+        END IF;
+
+        -- Compute the average weighted score for the user
+        CALL ComputeAverageWeightedScoreForUser(user_id);
+    END LOOP;
+
+    -- Close the cursor
+    CLOSE cur;
 END $$
 DELIMITER ;
