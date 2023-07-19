16c1feabe6fbbdf05719da05e87dedd500fbbdd0
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-19 02:13:56 +0100

    implemented aggregation

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -3,7 +3,7 @@
 
 
 def top_students(mongo_collection):
-    """"""
+    """ Returns all students sorted by average score. """
     return mongo_collection.aggregate([
         {
             "$unwind": "$topics"
