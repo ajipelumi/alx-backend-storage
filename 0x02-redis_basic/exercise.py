30f86730343d80fb95bff9e0274fb49b7294eab8
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-20 01:07:40 +0100

    implemented writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Cache module. """
+import redis
+import uuid
+from typing import Any
+
+
+class Cache:
+    """ Cache class. """
+
+    def __init__(self):
+        """ Initialize class instance. """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """
+        Generate a random key, store input data in Redis using the random key
+        and return the key.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
