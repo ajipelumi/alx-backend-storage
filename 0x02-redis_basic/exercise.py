29620cb46296f7beae5767766ac64887b1f73215
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-07-20 12:08:57 +0100

    implemeted call history

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,6 +20,25 @@ def count_calls(method: Callable) -> Callable:
 
     return wrapper
 
+def call_history(method: Callable) -> Callable:
+    """
+    Store the history of inputs and outputs for a particular function.
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ Wrapper function. """
+        input = str(args)
+        input_key = method.__qualname__ + ":inputs"
+        self._redis.rpush(input_key, input)
+
+        output = str(method(self, *args, **kwargs))
+        output_key = method.__qualname__ + ":outputs"
+        self._redis.rpush(output_key, output)
+
+        return output
+
+    return wrapper
+
 
 class Cache:
     """ Cache class. """
@@ -30,6 +49,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Generate a random key, store input data in Redis using the random key
